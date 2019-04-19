0ff036eb68222a2110bf6860db69fefedfb7e072
ldk <driverliu2018@gmail.com>
2019-04-19 17:56:16 +0800

    添加了韩佳欣的代码进去，itk的代码就是个坑 但是在我这里是能跑通的

--- itkHepaticVascularPreprocessFilter.cpp
+++ itkHepaticVascularPreprocessFilter.cpp
@@ -6,7 +6,7 @@
 namespace itk
 {
 	template< typename TInputImage, typename TOutputImage >
-	void 
+	void
 		HepaticVascularPreprocessFilter< TInputImage, TOutputImage >
 		::GenerateData()
 	{
@@ -30,15 +30,36 @@ namespace itk
 		m_MaximumProjectionImage->DisconnectPipeline();
 
 		// Training the GMM
-		this->TrainingGMM();
+		// Enable which method
+		if (m_UseItkFilter)
+		{
+			this->TrainingGMM();
+			std::sort(m_Components.begin(), m_Components.end());
+
+			m_ThresholdAbove = m_Components[m_NumOfComponents - 1].first + 2 * m_Components[m_NumOfComponents - 1].second;
+			m_ThresholdBelow = m_Components[0].first - 2 * m_Components[0].second;
+
+			m_Alpha = ((m_Components[m_NumOfComponents - 1].first - m_Components[(int)std::floor(m_NumOfComponents / 2)].first)) / 2;
+			m_Beta = ((m_Components[m_NumOfComponents - 1].first + m_Components[(int)std::floor(m_NumOfComponents / 2)].first)) / 2;
+		}
+		else
+		{
+			double *data = m_MaximumProjectionImage->GetBufferPointer();
+			int size = m_MaximumProjectionImage->GetLargestPossibleRegion().GetNumberOfPixels();
+			GMM *m_GMM = new GMM(1, m_NumOfComponents);
+			m_GMM->Train(data, size);
+
+			m_ThresholdAbove = m_GMM->Mean(0)[0] + 2 * std::sqrt(m_GMM->Variance(0)[0]);
+			m_ThresholdBelow = m_GMM->Mean(m_NumOfComponents - 1)[0] - 2 * std::sqrt(m_GMM->Variance(m_NumOfComponents - 1)[0]);
+
+			m_Alpha = ((m_GMM->Mean(0)[0] - m_GMM->Mean((int)std::ceil(m_NumOfComponents / 2))[0])) / 2;
+			m_Beta = ((m_GMM->Mean(0)[0] + m_GMM->Mean((int)std::ceil(m_NumOfComponents / 2))[0])) / 2;
+		}
 
 		/** Stupid filter forces me to call it twice. */
 		typedef typename ThresholdImageFilter<InputImageType>      ThresholdImageFilterType;
 		ThresholdImageFilterType::Pointer thresholdImageFilterAbove = ThresholdImageFilterType::New();
 		ThresholdImageFilterType::Pointer thresholdImageFilterBelow = ThresholdImageFilterType::New();
-
-		m_ThresholdAbove = m_ComponentMeanValues[m_NumOfComponents - 1] + 2 * m_ComponentStdValues[m_NumOfComponents - 1];
-		m_ThresholdBelow = m_ComponentMeanValues[0] - 2 * m_ComponentStdValues[0];
 
 		thresholdImageFilterAbove->SetInput(this->GetInput());
 		thresholdImageFilterAbove->ThresholdAbove(m_ThresholdAbove);
@@ -54,9 +75,6 @@ namespace itk
 		typedef typename SigmoidImageFilter<InputImageType, OutputImageType>      SigmoidImageFilterType;
 		SigmoidImageFilterType::Pointer sigmoidImageFilter = SigmoidImageFilterType::New();
 
-		m_Alpha = ((m_ComponentMeanValues[m_NumOfComponents - 1] - m_ComponentMeanValues[(int)std::floor(m_NumOfComponents / 2)])) / 2;
-		m_Beta = ((m_ComponentMeanValues[m_NumOfComponents - 1] + m_ComponentMeanValues[(int)std::floor(m_NumOfComponents / 2)])) / 2;
-
 		sigmoidImageFilter->SetInput(thresholdImageFilterBelow->GetOutput());
 		sigmoidImageFilter->SetOutputMinimum(m_MinValue);
 		sigmoidImageFilter->SetOutputMaximum(2000);
@@ -70,7 +88,7 @@ namespace itk
 	}
 
 	template<typename TInputImage, typename TOutputImage>
-	void 
+	void
 		HepaticVascularPreprocessFilter<TInputImage, TOutputImage>
 		::TrainingGMM()
 	{
@@ -81,7 +99,7 @@ namespace itk
 		sample->SetMeasurementVectorSize(1);
 
 		ImageRegionConstIterator<ProjectionImageType> it(m_MaximumProjectionImage, m_MaximumProjectionImage->GetLargestPossibleRegion());
-		for (it.GoToBegin();!it.IsAtEnd();++it)
+		for (it.GoToBegin(); !it.IsAtEnd(); ++it)
 		{
 			sample->PushBack(it.Get());
 		}
@@ -90,7 +108,7 @@ namespace itk
 		typedef typename itk::Array< RealType > ParametersType;
 		ParametersType params(2);
 		std::vector< ParametersType > initialParameters(m_NumOfComponents);
-		for (int i = 0; i < m_NumOfComponents ; i++)
+		for (int i = 0; i < m_NumOfComponents; i++)
 		{
 			params[0] = m_MaxValue / m_NumOfComponents * i;
 			params[1] = 100.0;
@@ -123,16 +141,14 @@ namespace itk
 
 		for (unsigned int i = 0; i < m_NumOfComponents; ++i)
 		{
-			//std::cout << "Cluster[" << i << "]" << std::endl;
-			//std::cout << "    Parameters:" << std::endl;
-			//std::cout << "         " << (components[i])->GetFullParameters()[0]
-			//	<< "         " << std::sqrt((components[i])->GetFullParameters()[1]) << std::endl;
-			//std::cout << "    Proportion: ";
-			//std::cout << "         " << estimator->GetProportions()[i] << std::endl;
+			std::cout << "Cluster[" << i << "]" << std::endl;
+			std::cout << "    Parameters:" << std::endl;
+			std::cout << "         " << (components[i])->GetFullParameters()[0]
+				<< "         " << std::sqrt((components[i])->GetFullParameters()[1]) << std::endl;
+			std::cout << "    Proportion: ";
+			std::cout << "         " << estimator->GetProportions()[i] << std::endl;
 
-			m_MixtureCoefficient.push_back(estimator->GetProportions()[i]);
-			m_ComponentMeanValues.push_back((components[i])->GetFullParameters()[0]);
-			m_ComponentStdValues.push_back(std::sqrt((components[i])->GetFullParameters()[1]));
+			m_Components.push_back(std::make_pair((components[i])->GetFullParameters()[0], std::sqrt((components[i])->GetFullParameters()[1])));
 		}
 
 	}
@@ -143,7 +159,7 @@ namespace itk
 		::PrintSelf(std::ostream & os, Indent indent) const
 	{
 		Superclass::PrintSelf(os, indent);
-		
+
 		os << indent << "Max value in the input image: "
 			<< m_MaxValue << std::endl;
 		os << indent << "Min value in the input image: "
